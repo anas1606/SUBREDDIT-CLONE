5847879bd8679481ba8b96c81ab736bf532bd756
anas1606 <anasloriya.info@gmail.com>
2021-01-13 15:52:51 +0530

    Final Commit

--- src/main/java/com/example/redditclone/repository/UserDetail.java
+++ src/main/java/com/example/redditclone/repository/UserDetail.java
@@ -3,8 +3,6 @@ import com.example.redditclone.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository("userdetail")
 public interface UserDetail extends JpaRepository<User,Long> {
     User findByUsername(String user);
